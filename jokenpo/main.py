32aa4c7bc1e408f0333582275cdc3faa3d41c9b3
isaacnwt <isaacnewton02@hotmail.com>
2022-02-02 11:17:01 -0300

    a few modifications; comments and lower function

--- jokenpo/main.py
+++ jokenpo/main.py
@@ -3,16 +3,20 @@ from random import choice
 line = "-" * 50
 
 signalsPt = ["pedra", "papel", "tesoura"]
-signalsEn = ["stone", "paper", "scissors"]
+#signalsEn = ["stone", "paper", "scissors"]
 
+
+# Returns the number of rounds
 def startsTheGame():
     print(line)
     print("JOKENPY")
     print(line)
 
     number_of_rounds = int(input("Set the number of rounds: "))
+    print(line)
     return number_of_rounds
 
+# Receives the player signal and choice random a signal
 def round(p1_points, p2_points):
     signal = input("Choose your option: ")
 
@@ -24,12 +28,12 @@ def round(p1_points, p2_points):
     print(choice(signalsPt).title())
     print(line)
 
-
-
-
+# Returns the winner
+#def comparesValues(p1_signal, p2_signal):
+#    p1_signal = p1_signal.lower()
 
 def checksAnswer(word, options):
-    if word in options:
+    if word.lower() in options:
         return True
     return False
 
@@ -44,7 +48,4 @@ comp_points = 0
 while rounds > round_count:
     round(player_points, comp_points)
     round_count+=1
-
-
-
-
+    
